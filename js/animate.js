f3bbd61d089ce4fa5854cb223ac8ed80808f9556
Arjun <junnaonly@gmail.com>
2014-12-15 22:16:52 +0200

    incoming works great

--- js/animate.js
+++ js/animate.js
@@ -1,12 +1,23 @@
 function incoming() {
-	$("#rect1")
-	.velocity({ x: "+=200"});
 	
-	$("#circle1")
-	.velocity({ cx: "+=200"});
+	setInterval(function in1() {
+  	$("#statustext")
+      	.velocity("transition.slideRightIn", 750)
+      	.delay(150)
+      	.velocity({ opacity: 0 }, 750);
+
+	return in1;
+	}(), 1500);
+
 	
-	$("#statustext")
-	.velocity({ x: "+=200"});
+	setInterval(function in2() {
+  	$("#rect1")
+      	.velocity("transition.slideRightIn", 750)
+      	.delay(150)
+      	.velocity({ opacity: 0 }, 750);
+
+	return in2;
+	}(), 1500);
 	
 	console.log('incoming');
 }
