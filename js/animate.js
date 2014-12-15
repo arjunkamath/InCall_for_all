b5a587ceef71c84903734ea62f29e174cc8cab43
Arjun <junnaonly@gmail.com>
2014-12-15 19:56:03 +0200

    finally understood referencing

--- js/animate.js
+++ js/animate.js
@@ -1,22 +1,54 @@
-function moveCircleA() {
-	$("rect")
+function incoming() {
+	$("#rect1")
 	.velocity({ x: "+=200"});
 	
-	$("acircle")
-	.velocity({ r: "+=200"});
+	$("#circle1")
+	.velocity({ cx: "+=200"});
 	
-	console.log('moved rect');
-}
-
-function moveText() {
-	$("h1")
-	.velocity({ x: "+=200", y: "25%" });
-	
-	console.log('moved text');
-}
-
-function moveNow() {
-	$("element1")
+	$("#statustext")
 	.velocity({ x: "+=200"});
+	
+	console.log('incoming');
+}
 
+
+function connected() {
+	$("#rect1")
+	.velocity({ x: "+=200"});
+	
+	$("#circle1")
+	.velocity({ cx: "+=200"});
+	
+	$("#statustext")
+	.velocity({ x: "+=200"});
+	
+	console.log('connected');
+}
+
+
+function ongoing() {
+	$("#rect1")
+	.velocity({ x: "+=200"});
+	
+	$("#circle1")
+	.velocity({ cx: "+=200"});
+	
+	$("#statustext")
+	.velocity({ x: "+=200"});
+	
+	console.log('ongoing');
+}
+
+
+function disconnected() {
+	$("#rect1")
+	.velocity({ x: "+=200"});
+	
+	$("#circle1")
+	.velocity({ cx: "+=200"});
+	
+	$("#statustext")
+	.velocity({ x: "+=200"});
+	
+	console.log('disconnected');
 }
